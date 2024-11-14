1383766846e22c63154369056e850e57f94b64ee
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-11-14 02:07:34 +0100

    Add information to the commands

--- src/args.rs
+++ src/args.rs
@@ -11,18 +11,29 @@ pub struct Args {
 
 #[derive(Debug, Subcommand)]
 pub enum OperationMode {
+    /// Encode a message into a chunk
     Encode {
+        /// Image path to encode message into
         path: PathBuf,
+        /// Chunk type 4 letters alphabetical case sensitive to embed message into
         chunk_type: String,
+        /// The message to embed
         message: String,
+        /// Optinal output file to use instead of overwriting original
         output_file: Option<PathBuf>,
     },
+    /// Decode an image to read an embeded message from it
     Decode {
+        /// Image path to read message from
         path: PathBuf,
+        /// Chunk type 4 letters alphabetical case sensitive to embed message into
         chunk_type: String,
     },
+    /// Remove a chunk that contains a message
     Remove {
+        /// Image path to remove chunk from
         path: PathBuf,
+        /// Chunk type 4 letters alphabetical case sensitive to embed message into
         chunk_type: String,
     },
 }
